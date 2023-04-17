13817505179a60c7cde2001a02a43ef6fb9c37c8
OPastran02 <opastran02@gmail.com>
2023-04-16 21:26:42 -0300

    some fixes

--- showFormDownload.js
+++ showFormDownload.js
@@ -1,7 +1,13 @@
 const downloadReportButton = document.getElementById('download-report');
 const downloadForm = document.getElementById('download-form');
+const viewIcon = document.querySelector('.view-icon');
 
 downloadReportButton.addEventListener('click', function() {
     downloadReportButton.classList.add('hidden');
     downloadForm.classList.remove('hidden');
+});
+
+viewIcon.addEventListener('click', function() {
+    downloadForm.classList.add('hidden');
+    downloadReportButton.classList.remove('hidden');
 });
